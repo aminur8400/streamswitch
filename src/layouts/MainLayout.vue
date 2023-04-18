8b1f69554c112d195e586a57551138d5231eb4b1
aminur8400 <aminur.rahman8400@gmail.com>
2023-04-18 17:45:45 +0600

    second commit

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -6,19 +6,19 @@
     <q-page-container>
       <q-page class="row justify-center item-center">
         <router-view />
-      </q-page>      
+      </q-page>
     </q-page-container>
 
   </q-layout>
 </template>
 <style>
 .nav-background-img {
-    background-image: url('../assets/Portrait_dot_040823_background.jpg');
-    background-size: cover;
-    width: 100%;
-    height: 100vh;
-    position: fixed;
-    background-repeat: no-repeat;
-    background-position: center center;
+  background-image: url('../assets/Portrait_dot_040823_background.jpg');
+  background-size: cover;
+  width: 100%;
+  height: 100vh;
+  position: fixed;
+  background-repeat: no-repeat;
+  background-position: 0 0;
 }
 </style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -5,112 +5,109 @@
     </div>
   </div>
   <div>
-    <div class="row q-pa-sm q-ma-sm justify-center item-center outer-design">
-      <div class="q-px-xs q-my-xs">
-        <q-btn size="xs" color="white" text-color="black" label="Cam 1" />
-      </div>
-      <div class="q-px-xs q-my-xs">
-        <q-btn size="xs" color="white" text-color="black" label="Cam 2" />
-      </div>
-      <div class="q-px-xs q-my-xs">
-        <q-btn size="xs" color="white" text-color="black" label="Cam 3" />
-      </div>
-      <div class="q-px-xs q-my-xs">
-        <q-btn size="xs" color="white" text-color="black" label="Cam 4" />
-      </div>
-      <div class="q-px-xs q-my-xs">
-        <q-btn size="xs" color="white" text-color="black" label="Cam 5" />
-      </div>
-      <div class="q-px-xs q-my-xs">
-        <q-btn size="xs" color="white" text-color="black" label="Cam 6" />
-      </div>
-      <div class="q-px-xs q-my-xs">
-        <q-btn size="xs" color="white" text-color="black" label="Cam 7" />
-      </div>
-      <div class="q-px-xs q-my-xs">
-        <q-btn size="xs" color="white" text-color="black" label="Cam 8" />
-      </div>
-      <div class="q-px-xs q-my-xs">
-        <q-btn size="xs" color="white" text-color="black" label="Cam 9" />
-      </div>
-      <div class="q-px-xs q-my-xs qbtn-size">
-        <q-btn size="xs" color="white" text-color="black" label="Cam 10" />
-      </div>
-      <div class="q-px-xs q-my-xs">
-        <q-btn size="xs" color="white" text-color="black" label="Cam 1" />
-      </div>
-      <div class="q-px-xs q-my-xs">
-        <q-btn size="xs" color="white" text-color="black" label="Cam 2" />
-      </div>
-      <div class="q-px-xs q-my-xs">
-        <q-btn size="xs" color="white" text-color="black" label="Cam 3" />
-      </div>
-      <div class="q-px-xs q-my-xs">
-        <q-btn size="xs" color="white" text-color="black" label="Cam 4" />
-      </div>
-      <div class="q-px-xs q-my-xs">
-        <q-btn size="xs" color="white" text-color="black" label="Cam 5" />
-      </div>
-      <div class="q-px-xs q-my-xs">
-        <q-btn size="xs" color="white" text-color="black" label="Cam 6" />
-      </div>
-      <div class="q-px-xs q-my-xs">
-        <q-btn size="xs" color="white" text-color="black" label="Cam 7" />
-      </div>
-      <div class="q-px-xs q-my-xs">
-        <q-btn size="xs" color="white" text-color="black" label="Cam 8" />
-      </div>
-      <div class="q-px-xs q-my-xs">
-        <q-btn size="xs" color="white" text-color="black" label="Cam 9" />
-      </div>
-      <div class="q-px-xs q-my-xs">
-        <q-btn size="xs" color="white" text-color="black" label="Cam 10" />
-      </div>
-      <div class="q-px-xs q-my-xs">
-        <q-btn size="xs" color="white" text-color="black" label="Cam 1" />
-      </div>
-      <div class="q-px-xs q-my-xs">
-        <q-btn size="xs" color="white" text-color="black" label="Cam 2" />
-      </div>
-      <div class="q-px-xs q-my-xs">
-        <q-btn size="xs" color="white" text-color="black" label="Cam 3" />
-      </div>
-      <div class="q-px-xs q-my-xs">
-        <q-btn size="xs" color="white" text-color="black" label="Cam 4" />
-      </div>
-      <div class="q-px-xs q-my-xs">
-        <q-btn size="xs" color="white" text-color="black" label="Cam 5" />
-      </div>
-      <div class="q-px-xs q-my-xs">
-        <q-btn size="xs" color="white" text-color="black" label="Cam 6" />
-      </div>
-      <div class="q-px-xs q-my-xs">
-        <q-btn size="xs" color="white" text-color="black" label="Cam 7" />
-      </div>
-      <div class="q-px-xs q-my-xs">
-        <q-btn size="xs" color="white" text-color="black" label="Cam 8" />
-      </div>
-      <div class="q-px-xs q-my-xs">
-        <q-btn size="xs" color="white" text-color="black" label="Cam 9" />
-      </div>
-      <div class="q-px-xs q-my-xs">
-        <q-btn size="xs" color="white" text-color="black" label="Cam 10" />
+    <div class="row q-px-md">
+      <div class="cam-container q-pa-md">
+        <div class="cam-container-border q-pa-xs">
+          <div class="row justify-evenly item-center cam-el q-pa-sm">
+            <div class="q-px-xs q-my-xs">
+              <q-btn size="sm" color="white" text-color="white" label="Cam 1" />
+            </div>
+            <div class="q-px-xs q-my-xs">
+              <q-btn size="sm" color="white" text-color="white" label="Cam 2" />
+            </div>
+            <div class="q-px-xs q-my-xs">
+              <q-btn size="sm" color="white" text-color="white" label="Cam 3" />
+            </div>
+            <div class="q-px-xs q-my-xs">
+              <q-btn size="sm" color="white" text-color="white" label="Cam 4" />
+            </div>
+            <div class="q-px-xs q-my-xs">
+              <q-btn size="sm" color="white" text-color="white" label="Cam 5" />
+            </div>
+            <div class="q-px-xs q-my-xs">
+              <q-btn size="sm" color="white" text-color="white" label="Cam 6" />
+            </div>
+            <div class="q-px-xs q-my-xs">
+              <q-btn size="sm" color="white" text-color="white" label="Cam 7" />
+            </div>
+            <div class="q-px-xs q-my-xs">
+              <q-btn size="sm" color="white" text-color="white" label="Cam 8" />
+            </div>
+            <div class="q-px-xs q-my-xs">
+              <q-btn size="sm" color="white" text-color="white" label="Cam 9" />
+            </div>
+            <div class="q-px-xs q-my-xs qbtn-size">
+              <q-btn size="sm" color="white" text-color="white" label="Cam 10" />
+            </div>
+            <div class="q-px-xs q-my-xs">
+              <q-btn size="sm" color="white" text-color="white" label="Cam 1" />
+            </div>
+            <div class="q-px-xs q-my-xs">
+              <q-btn size="sm" color="white" text-color="white" label="Cam 2" />
+            </div>
+            <div class="q-px-xs q-my-xs">
+              <q-btn size="sm" color="white" text-color="white" label="Cam 3" />
+            </div>
+            <div class="q-px-xs q-my-xs">
+              <q-btn size="sm" color="white" text-color="white" label="Cam 4" />
+            </div>
+            <div class="q-px-xs q-my-xs">
+              <q-btn size="sm" color="white" text-color="white" label="Cam 5" />
+            </div>
+            <div class="q-px-xs q-my-xs">
+              <q-btn size="sm" color="white" text-color="white" label="Cam 6" />
+            </div>
+            <div class="q-px-xs q-my-xs">
+              <q-btn size="sm" color="white" text-color="white" label="Cam 7" />
+            </div>
+            <div class="q-px-xs q-my-xs">
+              <q-btn size="sm" color="white" text-color="white" label="Cam 8" />
+            </div>
+            <div class="q-px-xs q-my-xs">
+              <q-btn size="sm" color="white" text-color="white" label="Cam 9" />
+            </div>
+            <div class="q-px-xs q-my-xs">
+              <q-btn size="sm" color="white" text-color="white" label="Cam 10" />
+            </div>
+            <div class="q-px-xs q-my-xs">
+              <q-btn size="sm" color="white" text-color="white" label="Cam 1" />
+            </div>
+            <div class="q-px-xs q-my-xs">
+              <q-btn size="sm" color="white" text-color="white" label="Cam 2" />
+            </div>
+            <div class="q-px-xs q-my-xs">
+              <q-btn size="sm" color="white" text-color="white" label="Cam 3" />
+            </div>
+            <div class="q-px-xs q-my-xs">
+              <q-btn size="sm" color="white" text-color="white" label="Cam 4" />
+            </div>
+            <div class="q-px-xs q-my-xs">
+              <q-btn size="sm" color="white" text-color="white" label="Cam 5" />
+            </div>
+            <div class="q-px-xs q-my-xs">
+              <q-btn size="sm" color="white" text-color="white" label="Cam 6" />
+            </div>
+            <div class="q-px-xs q-my-xs">
+              <q-btn size="sm" color="white" text-color="white" label="Cam 7" />
+            </div>
+            <div class="q-px-xs q-my-xs">
+              <q-btn size="sm" color="white" text-color="white" label="Cam 8" />
+            </div>
+            <div class="q-px-xs q-my-xs">
+              <q-btn size="sm" color="white" text-color="white" label="Cam 9" />
+            </div>
+            <div class="q-px-xs q-my-xs">
+              <q-btn size="sm" color="white" text-color="white" label="Cam 10" />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div> 
-  <div class="row footer-seciton bg-black section-no-wrap">
-        <q-img
-          src="../assets/FG_Badge_Logo.png"
-          style="height: 100%; max-width: 100%"
-        >
-        </q-img>
-        <q-img
-          src="../assets/VCR_pms285_logo.jpg"
-          style="height: 100%; max-width: 100%"
-        >
-        </q-img>
   </div>
-
+  <div class="row no-wrap justify-between footer-seciton bg-black q-px-md q-py-sm">
+    <img src="../assets/VCR_pms285_logo.jpg" />
+    <img src="../assets/FG_Badge_Logo.png" />
+  </div>
 </template>
 
 <script>
@@ -122,35 +119,63 @@ export default defineComponent({
 </script>
 <style>
 .footer-seciton {
-    width: 100%;
-    height: 65px;
-    position: fixed;
-    bottom: 82px;
+  width: 100%;
+  height: 65px;
+  position: fixed;
+  bottom: 82px;
 }
+
 .qimage-section {
   height: 100%;
   width: 100%;
 }
-.text-color{
+
+.text-color {
   color: #ffffff;
 }
+
 .bg-color {
   color: #4f4f58;
 }
+
 img.q-img__image.q-img__image--with-transition.q-img__image--loaded {
-    width: auto;
+  width: auto;
 }
+
 .section-no-wrap {
   flex-wrap: nowrap;
 }
-.outer-design {
-  background-color: #929396;
-    padding: 23px;
-    margin: 12px;
+
+.cam-el {
+  border-radius: 5px;
+  background-color: #262231;
 }
+
 .c1 {
-    display: flex;
-    width: 100%;
-    justify-content: center;
+  display: flex;
+  width: 100%;
+  justify-content: center;
+}
+
+.cam-container {
+  border-radius: 10px;
+  background: rgb(146, 147, 151);
+  background: -moz-linear-gradient(180deg, rgba(146, 147, 151, 1) 0%, rgba(79, 79, 85, 1) 100%);
+  background: -webkit-linear-gradient(180deg, rgba(146, 147, 151, 1) 0%, rgba(79, 79, 85, 1) 100%);
+  background: linear-gradient(180deg, rgba(146, 147, 151, 1) 0%, rgba(79, 79, 85, 1) 100%);
+  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#929397", endColorstr="#4f4f55", GradientType=1);
+}
+
+.cam-container-border {
+  border-radius: 7px;
+  background: rgb(99, 95, 110);
+  background: -moz-linear-gradient(180deg, rgba(99, 95, 110, 1) 0%, rgba(99, 95, 110, 1) 47%, rgba(73, 69, 83, 1) 100%);
+  background: -webkit-linear-gradient(180deg, rgba(99, 95, 110, 1) 0%, rgba(99, 95, 110, 1) 47%, rgba(73, 69, 83, 1) 100%);
+  background: linear-gradient(180deg, rgba(99, 95, 110, 1) 0%, rgba(99, 95, 110, 1) 47%, rgba(73, 69, 83, 1) 100%);
+  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#635f6e", endColorstr="#494553", GradientType=1);
+}
+
+.cam-el .q-btn {
+  background: #4f4f59 !important;
 }
 </style>
