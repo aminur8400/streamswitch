5a00a7072b711953ba3fbab9bc2a0c8405ee8262
aminur8400 <aminur.rahman8400@gmail.com>
2023-04-18 17:36:46 +0600

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="nav-background-img">
+  </div>
+  <q-layout view="hHh lpR fFf">
+
+    <q-page-container>
+      <q-page class="row justify-center item-center">
+        <router-view />
+      </q-page>      
+    </q-page-container>
+
+  </q-layout>
+</template>
+<style>
+.nav-background-img {
+    background-image: url('../assets/Portrait_dot_040823_background.jpg');
+    background-size: cover;
+    width: 100%;
+    height: 100vh;
+    position: fixed;
+    background-repeat: no-repeat;
+    background-position: center center;
+}
+</style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,156 @@
+<template>
+  <div class="c1 text-white q-pa-sm q-ma-xs no-box-shadow">
+    <div class="streaming-items">
+      <h5>Streaming Goes here!</h5>
+    </div>
+  </div>
+  <div>
+    <div class="row q-pa-sm q-ma-sm justify-center item-center outer-design">
+      <div class="q-px-xs q-my-xs">
+        <q-btn size="xs" color="white" text-color="black" label="Cam 1" />
+      </div>
+      <div class="q-px-xs q-my-xs">
+        <q-btn size="xs" color="white" text-color="black" label="Cam 2" />
+      </div>
+      <div class="q-px-xs q-my-xs">
+        <q-btn size="xs" color="white" text-color="black" label="Cam 3" />
+      </div>
+      <div class="q-px-xs q-my-xs">
+        <q-btn size="xs" color="white" text-color="black" label="Cam 4" />
+      </div>
+      <div class="q-px-xs q-my-xs">
+        <q-btn size="xs" color="white" text-color="black" label="Cam 5" />
+      </div>
+      <div class="q-px-xs q-my-xs">
+        <q-btn size="xs" color="white" text-color="black" label="Cam 6" />
+      </div>
+      <div class="q-px-xs q-my-xs">
+        <q-btn size="xs" color="white" text-color="black" label="Cam 7" />
+      </div>
+      <div class="q-px-xs q-my-xs">
+        <q-btn size="xs" color="white" text-color="black" label="Cam 8" />
+      </div>
+      <div class="q-px-xs q-my-xs">
+        <q-btn size="xs" color="white" text-color="black" label="Cam 9" />
+      </div>
+      <div class="q-px-xs q-my-xs qbtn-size">
+        <q-btn size="xs" color="white" text-color="black" label="Cam 10" />
+      </div>
+      <div class="q-px-xs q-my-xs">
+        <q-btn size="xs" color="white" text-color="black" label="Cam 1" />
+      </div>
+      <div class="q-px-xs q-my-xs">
+        <q-btn size="xs" color="white" text-color="black" label="Cam 2" />
+      </div>
+      <div class="q-px-xs q-my-xs">
+        <q-btn size="xs" color="white" text-color="black" label="Cam 3" />
+      </div>
+      <div class="q-px-xs q-my-xs">
+        <q-btn size="xs" color="white" text-color="black" label="Cam 4" />
+      </div>
+      <div class="q-px-xs q-my-xs">
+        <q-btn size="xs" color="white" text-color="black" label="Cam 5" />
+      </div>
+      <div class="q-px-xs q-my-xs">
+        <q-btn size="xs" color="white" text-color="black" label="Cam 6" />
+      </div>
+      <div class="q-px-xs q-my-xs">
+        <q-btn size="xs" color="white" text-color="black" label="Cam 7" />
+      </div>
+      <div class="q-px-xs q-my-xs">
+        <q-btn size="xs" color="white" text-color="black" label="Cam 8" />
+      </div>
+      <div class="q-px-xs q-my-xs">
+        <q-btn size="xs" color="white" text-color="black" label="Cam 9" />
+      </div>
+      <div class="q-px-xs q-my-xs">
+        <q-btn size="xs" color="white" text-color="black" label="Cam 10" />
+      </div>
+      <div class="q-px-xs q-my-xs">
+        <q-btn size="xs" color="white" text-color="black" label="Cam 1" />
+      </div>
+      <div class="q-px-xs q-my-xs">
+        <q-btn size="xs" color="white" text-color="black" label="Cam 2" />
+      </div>
+      <div class="q-px-xs q-my-xs">
+        <q-btn size="xs" color="white" text-color="black" label="Cam 3" />
+      </div>
+      <div class="q-px-xs q-my-xs">
+        <q-btn size="xs" color="white" text-color="black" label="Cam 4" />
+      </div>
+      <div class="q-px-xs q-my-xs">
+        <q-btn size="xs" color="white" text-color="black" label="Cam 5" />
+      </div>
+      <div class="q-px-xs q-my-xs">
+        <q-btn size="xs" color="white" text-color="black" label="Cam 6" />
+      </div>
+      <div class="q-px-xs q-my-xs">
+        <q-btn size="xs" color="white" text-color="black" label="Cam 7" />
+      </div>
+      <div class="q-px-xs q-my-xs">
+        <q-btn size="xs" color="white" text-color="black" label="Cam 8" />
+      </div>
+      <div class="q-px-xs q-my-xs">
+        <q-btn size="xs" color="white" text-color="black" label="Cam 9" />
+      </div>
+      <div class="q-px-xs q-my-xs">
+        <q-btn size="xs" color="white" text-color="black" label="Cam 10" />
+      </div>
+    </div>
+  </div> 
+  <div class="row footer-seciton bg-black section-no-wrap">
+        <q-img
+          src="../assets/FG_Badge_Logo.png"
+          style="height: 100%; max-width: 100%"
+        >
+        </q-img>
+        <q-img
+          src="../assets/VCR_pms285_logo.jpg"
+          style="height: 100%; max-width: 100%"
+        >
+        </q-img>
+  </div>
+
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
+<style>
+.footer-seciton {
+    width: 100%;
+    height: 65px;
+    position: fixed;
+    bottom: 82px;
+}
+.qimage-section {
+  height: 100%;
+  width: 100%;
+}
+.text-color{
+  color: #ffffff;
+}
+.bg-color {
+  color: #4f4f58;
+}
+img.q-img__image.q-img__image--with-transition.q-img__image--loaded {
+    width: auto;
+}
+.section-no-wrap {
+  flex-wrap: nowrap;
+}
+.outer-design {
+  background-color: #929396;
+    padding: 23px;
+    margin: 12px;
+}
+.c1 {
+    display: flex;
+    width: 100%;
+    justify-content: center;
+}
+</style>
